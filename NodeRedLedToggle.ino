deb7def89d67ccf046e600ce18eb963424dfabd7
Ehab Magdy <132620660+EhabMagdyy@users.noreply.github.com>
2024-05-13 23:39:48 +0300

    Update.ino

--- NodeRedLedToggle.ino
+++ NodeRedLedToggle.ino
@@ -1,5 +1,10 @@
 
-#include <WiFi.h>  
+#if defined(ESP32)
+#include <WiFi.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#endif 
+
 #include <PubSubClient.h>
 
 #define LedPin      2                           // ESP32 in-board led pin
